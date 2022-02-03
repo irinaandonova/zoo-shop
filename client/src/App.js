d613ec48b6b69f3dcdb8f24fe416b7e25548687f
irinaandonova <irinaandonova97@gmail.com>
2022-02-03 18:20:46 +0200

    Add header to app

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home.js';
 import Login from './components/Login/Login.js';
 import Register from './components/Register/Register.js';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/auth/login' element={<Login />} />
