ab38806aaea52a70659183c7b8eef58f63e9ee43
irinaandonova <irinaandonova97@gmail.com>
2022-02-03 13:47:32 +0200

    Add controllers to router

--- server/router.js
+++ server/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const authController = require('./controllers/authController.js');
+const homeController = require('./controllers/homeController.js');
+const productController = require('./controllers/productController.js');
 
+router.use(homeController);
 router.use('/auth', authController);
+router.use('/product', productController);
 
 module.exports = router;
