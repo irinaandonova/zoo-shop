4adb5658fce044ff30800d1dae593fdf9672c859
irinaandonova <irinaandonova97@gmail.com>
2022-02-03 18:09:46 +0200

    Create home page

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import Products from "../Products/Products.js";
+
+const Home = () => {
+    return (
+        <section className="home-page">
+            
+            <article className="products">
+                <Products />
+            </article>
+        </section>
+    )
+}
+
+export default Home;
