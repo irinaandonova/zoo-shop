6d2926bf9f34de5d108c568e07d593195ba34d5a
irinaandonova <irinaandonova97@gmail.com>
2022-02-06 20:57:03 +0200

    Delete homeController.js

--- server/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const productService = require('../services/productService.js');
-
-router.get('/', async (req, res) => {
-    let products = await productService.getAll();
-    res.json(products)
-})
-
-module.exports = router;
