308a4c2042d5d5d43104d05dfd3acec7d77bce03
irinaandonova <irinaandonova97@gmail.com>
2022-02-06 20:53:39 +0200

    Change component name

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home.js';
 import Login from './components/Login/Login.js';
 import Register from './components/Register/Register.js';
 import Header from './components/Header/Header.js';
+import Dashboard from './components/Home/Dashboard.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Dashboard />} />
         <Route path='/auth/login' element={<Login />} />
         <Route path='/auth/register' element={<Register />} />
+        <Route path='/cats' element={<Dashboard />} />
+        <Route path='/dogs' element={<Dashboard />} />
+        <Route path='/roden' element={<Dashboard />} />
+        <Route path='/others' element={<Dashboard />} />
       </Routes>
     </div>
   );
