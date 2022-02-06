fa47e6ce3d3108898d8552ab5f505fac5b1ad6a0
irinaandonova <irinaandonova97@gmail.com>
2022-02-06 20:55:41 +0200

    Create universal dashboard component

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import productService from "../../services/productsService.js";
+import Product from "../ProductCard/ProductCard.js";
+
+const Dashboard = () => {
+    const [products, setProducts] = useState([]);
+    const location = useLocation();
+       
+    useEffect(() => {
+        productService.getProducts()
+            .then(res => res.json())
+            .then(result => setProducts(result))
+            .catch(err => console.log(err))
+    }, [location]);
+
+    return (
+        <section className="home-page">
+            <article className="products">
+                <ul className="all-products">
+                {products.map(x => <Product product={x} key={x._id}/>)}
+                </ul>
+            </article>
+        </section>
+    )
+}
+
+export default Dashboard;
