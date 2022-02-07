14a3ab6cc9d652c9665a774c30ece4e36e390c96
irinaandonova <irinaandonova97@gmail.com>
2022-02-07 07:36:23 +0200

    Check if product array is empty and get path

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import productService from "../../services/productsService.js";
+import * as productService from "../../services/productsService.js";
 import Product from "../Product/Product.js";
 
 const Dashboard = () => {
     const [products, setProducts] = useState([]);
     const location = useLocation();
-       
+    
+    const path = location.pathname.split('/')[1];
+    console.log(path);
+
     useEffect(() => {
-        productService.getProducts()
+        productService.getProducts(path)
             .then(res => res.json())
             .then(result => setProducts(result))
             .catch(err => console.log(err))
-    }, [location]);
+    }, [path]);
 
     return (
         <section className="home-page">
             <article className="products">
                 <ul className="all-products">
-                {products.map(x => <Product product={x} key={x._id}/>)}
+                    {products.length > 0 ? 
+                    products.map(x => <Product product={x} key={x._id} />)
+                    :
+                    <li>There is no <span>{path} </span>items at the moment</li>}
                 </ul>
             </article>
         </section>
