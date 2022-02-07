38d30bab4be8d76905f0c22b956633f5b419e85d
irinaandonova <irinaandonova97@gmail.com>
2022-02-07 12:51:30 +0200

    Revert "Change server response and implement error handling"
    This reverts commit 601dae6be8997d6df69f3f5dafa894cda70ceee6.

--- server/services/productService.js
+++ server/services/productService.js
@@ -1,42 +1,32 @@
 const Product = require("../models/Product.js")
 
-const getAll = async () => {
-    try {
-        let result = await Product.find({}).lean();
-        res.status(201).json({ result });
+const getAll = async() => {
+    try{
+    let result  = await Product.find({}).lean();
+        return result;
     }
-    catch (err) {
+    catch(err) {
         console.log(err);
-        res.status(501).json('Connot fetch data!');
+        return {status: 'error'}
     }
 }
 
-const getOne = async (id) => {
-    try {
-        let product = await Product.findById(id).lean();
-        res.status(201).json({ product })
-
-    }
-    catch (err) {
-        console.log(err);
-        res.status(501).json('Connot fetch data!');
-    }
+const getOne = async(id) => {
+    let product =  await Product.findById(id).lean();
+    
+    return product;
 }
 
 const getByType = async (animal) => {
-    try {
-        const allProducts = await getAll();
-        const products = [];
-        allProducts.forEach(x => {
-            if (x.animal === animal) {
-                products.push(x);
-            }
-        })
-    }
-    catch (err) {
-        console.log(err);
-        res.status(501).json('Connot fetch data!');
-    }
+    const allProducts = await getAll();  
+    const products = [];
+    allProducts.forEach(x=> {
+        if(x.animal === animal) {
+            products.push(x);
+        }
+    })
+    return products;  
+   
 }
 const productService = {
     getAll,
