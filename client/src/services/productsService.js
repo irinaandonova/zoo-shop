d6b1f9af4046f91e6903b148b78771b8e3564fb7
irinaandonova <irinaandonova97@gmail.com>
2022-02-07 07:38:06 +0200

    Separate get all request from get by animal

--- client/src/services/productsService.js
+++ client/src/services/productsService.js
@@ -1,19 +1,23 @@
 const baseUrl = 'http://localhost:4000/product';
 
-const getProducts = async() => {
-    console.log('here');
-    try{
-        let response = await fetch(`${baseUrl}`);
-        return response;       
+exports.getProducts = async (path) => {
+    if (path) {
+        try {
+            let response = await fetch(`${baseUrl}/${path}`);
+            return response;
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
-    catch(err) {
-        console.log(err);
-    }   
+    else{
+        try {
+            let response = await fetch(`${baseUrl}`);
+            return response;
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
 }
 
-
-const productService = {
-    getProducts
-}
-
-export default productService;
