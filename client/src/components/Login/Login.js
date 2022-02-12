69cf5ded80a9b57515ace121f6f73f9b120d822f
irinaandonova <irinaandonova97@gmail.com>
2022-02-12 17:30:01 +0200

    Update login logic

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,26 @@
-import { Link } from "react-router-dom";
-
+import {  useContext } from "react";
+import { Link,  useNavigate } from "react-router-dom";
+import AuthContext from '../../context/AuthContext.js';
 const Login = () => {
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        let response = await login({ email, password });
+        if(response.status === 'ok') {
+            navigate('/')
+        }
+        else {
+            navigate('/auth/register')
+        }
+
+    }
     return (
         <section className="auth-form">
-            <form className="loginForm">
+            <form className="loginForm" onSubmit={onSubmitHandler}>
                 <ul className="form-input">
                     <li>
                         <label htmlFor="email">E-mail:</label>
@@ -25,5 +42,4 @@ const Login = () => {
         </section>
     )
 }
-
 export default Login;
