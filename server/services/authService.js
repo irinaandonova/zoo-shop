8c3808ddc07e15597a2c90f86263b3ba61c03234
irinaandonova <irinaandonova97@gmail.com>
2022-02-12 17:32:33 +0200

    Change register logic and create login logic in server side

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,20 +1,51 @@
 const User = require('../models/User.js');
+const jwt = require('jsonwebtoken');
+const SECRET = '1376E6C33989144E433CAC15D747C';
 
-const register = async (userInfo) => {
+
+const register = async ({ email, firstName, lastName, phoneNumber, town, address, password, rePassword }) => {
+    console.log(`user - ${email}`);
+
     try {
-        const user = new User(userInfo);
+        const user = new User({ email, firstName, lastName, phoneNumber, town, address, password, rePassword });
         await user.save();
-        return {user, status: 'ok'};
+        return { status: 'ok', user };
     }
     catch (err) {
         console.log(`Registration failure!`, err);
         return { status: 'error' };
     }
-
 }
 
+const login = async ({ email, password }) => {
+    try {
+        const user = await User.findOne({ email });
+
+        if (user.password === password) {
+
+            const token = jwt.sign(
+                {
+                    firstName: user.firstName,
+                    _id: user._id,
+                    email: user.email,
+                },
+                SECRET
+            )
+            return { status: 'ok', token, firstName: user.firstName, _id: user._id.toString() }
+        }
+        else {
+            return { status: 'err', token: false };
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return { status: 'error' };
+    }
+
+}
 const authService = {
-    register
+    register,
+    login
 }
 
 module.exports = authService;
