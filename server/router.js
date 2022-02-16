efc5061829b3285a9d504affc7fdccb251dbddcf
irinaandonova <irinaandonova97@gmail.com>
2022-02-16 21:11:45 +0200

    Delete useless console.log

--- server/router.js
+++ server/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+
 const authController = require('./controllers/authController.js');
 const productController = require('./controllers/productController.js');
-
+const profileController = require('./controllers/profileController.js');
 
 router.use('/auth', authController);
 router.use('/product', productController);
+router.use('/profile', profileController);
 
 module.exports = router;
