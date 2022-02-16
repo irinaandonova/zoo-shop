75499ab20d561ee30d1d9f1a503c56e8b6f535b0
irinaandonova <irinaandonova97@gmail.com>
2022-02-16 21:09:11 +0200

    Change profile path

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext.js"
 const Header = () => {
     const { userInfo, logout } = useContext(AuthContext);
-   console.log(userInfo);
-    
+    console.log(userInfo);
+
     return (
         <header id="site-header">
             <article className="upper-header">
                 <Link to="/" className="logo">ZOOLAND</Link>
                 {userInfo._id ?
-            <article className="user-header-article">
-            <p className="greeting">Здравейте, {userInfo.firstName}</p>
-            <Link to="/profile/:_id" className="profile">Към профила</Link>
-            <input type="button" onClick={logout} value="Изход" className="profile logout"/>
-            </article>
-            : 
-            <article className="guest-header-article">
-            <Link to="/auth/login" className="profile">Вход</Link>
-            <Link to="/auth/register" className="profile">Регистация</Link>
-            </article>
-            }
+                    <article className="user-header-article">
+                        <p className="greeting">Здравейте, {userInfo.firstName}</p>
+                        <Link to="/profile" className="profile">Към профила</Link>
+                        <input type="button" onClick={logout} value="Изход" className="profile logout" />
+                    </article>
+                    :
+                    <article className="guest-header-article">
+                        <Link to="/auth/login" className="profile">Вход</Link>
+                        <Link to="/auth/register" className="profile">Регистация</Link>
+                    </article>
+                }
             </article>
             <nav className="navbar">
                 <ul className="nav-list">
