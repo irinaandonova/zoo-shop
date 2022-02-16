66d6f9df90ebf7e4c0207811d4463715cf628b52
irinaandonova <irinaandonova97@gmail.com>
2022-02-16 21:11:12 +0200

    Change authentication logic by using authContext and add details route

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,34 @@ import Login from './components/Login/Login.js';
 import Register from './components/Register/Register.js';
 import Header from './components/Header/Header.js';
 import Dashboard from './components/Dashboard/Dashboard.js';
-import { useContext, useState } from 'react';
-import AuthContext from './context/AuthContext.js';
-function App() {
-  const initialValue = { firstName: '', email: '', _id: '' }
-  const [user, setUser] = useState(initialValue);
+import { AuthContextProvider } from './context/AuthContext.js';
+import MyProfile from './components/MyProfile/MyProfile.js';
+import EditProfile from './components/Edit Profile/EditProfile.js';
+import Details from './components/Details/Details.js';
 
-  
+function App() {
+
+
+
 
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/auth/login' element={<Login />} />
-        <Route path='/auth/register' element={<Register />} />
-        <Route path='/cat' element={<Dashboard />} />
-        <Route path='/dog' element={<Dashboard />} />
-        <Route path='/roden' element={<Dashboard />} />
-        <Route path='/other' element={<Dashboard />} />
-      </Routes>
+      <AuthContextProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/auth/login' element={<Login />} />
+          <Route path='/auth/register' element={<Register />} />
+          <Route path='/cat' element={<Dashboard />} />
+          <Route path='/dog' element={<Dashboard />} />
+          <Route path='/roden' element={<Dashboard />} />
+          <Route path='/other' element={<Dashboard />} />
+          <Route path='/profile' element={<MyProfile />}/>
+          <Route path="/profile/:userId/edit" element={<EditProfile />}/>
+          <Route path='/details/:productId' element={<Details />}/>
+        </Routes>
+      </AuthContextProvider>
+
     </div>
   );
 }
