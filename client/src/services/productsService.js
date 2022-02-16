d9ea319a8fbf137751c3d6a300c1ba67f1bb6ea4
irinaandonova <irinaandonova97@gmail.com>
2022-02-16 21:10:16 +0200

    Create get one product service in client side

--- client/src/services/productsService.js
+++ client/src/services/productsService.js
@@ -1,9 +1,9 @@
 const baseUrl = 'http://localhost:4000/product';
 
-exports.getProducts = async (path) => {
-    if (path) {
+exports.getProducts = async (animal) => {
+    if (animal) {
         try {
-            let response = await fetch(`${baseUrl}/${path}`);
+            let response = await fetch(`${baseUrl}/${animal}`);
             return response;
         }
         catch (err) {
@@ -16,8 +16,21 @@ exports.getProducts = async (path) => {
             return response;
         }
         catch (err) {
-            console.log(err);
+        throw new Error(err);
+
         }
+    }
+}
+
+exports.getProduct = async(id) => {
+    try{
+        let response = await fetch(`${baseUrl}/details/${id}`);
+        let result = await response.json();
+        
+        return  JSON.parse(result);
+    }
+    catch(err) {
+        throw new Error(err);
     }
 }
 
