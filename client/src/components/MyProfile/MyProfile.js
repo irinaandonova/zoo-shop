f14e1b601ac64cee149879187573e147864da2a0
irinaandonova <irinaandonova97@gmail.com>
2022-02-23 12:12:38 +0200

    Get user from authentication context

--- client/src/components/MyProfile/MyProfile.js
+++ client/src/components/MyProfile/MyProfile.js
@@ -1,28 +1,19 @@
 import { useContext, useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext.js";
 import profileService from "../../services/profileService.js";
 
 const MyProfile = () => {
-    const [info, setInfo] = useState({});
-    const { userInfo } = useContext(AuthContext);
-    const _id = userInfo._id;
-
-    useEffect(() => {
-        profileService.getProfile(_id)
-                     .then(res => setInfo(res))
-                     .catch(err => console.log(err))
-    }, [_id])
-    const navigate = useNavigate();
+   const { userInfo } = useContext(AuthContext);
     
     return(
         <section className="info-wrapper">
             <article className="info-article">
-            <label htmlFor="name">Име: {info.firstName} {info.lastName}</label>
-            <label htmlFor="email">Имейл: {info.email}</label>
-            <label htmlFor="name">Град: {info.town}</label>
-            <label htmlFor="name">Адрес: {info.address}</label>
-            <label htmlFor="name">Телефонен номер: {info.phoneNumber}</label>
+            <label htmlFor="name">Име: {userInfo.firstName} {userInfo.lastName}</label>
+            <label htmlFor="email">Имейл: {userInfo.email}</label>
+            <label htmlFor="name">Град: {userInfo.town}</label>
+            <label htmlFor="name">Адрес: {userInfo.address}</label>
+            <label htmlFor="name">Телефонен номер: {userInfo.phoneNumber}</label>
             </article>
             <Link to="/profile/:_id/edit" className="profile edit">Редакция на профил</Link>
         </section>
