01699c61e4236f8437b378be075d131c6563f340
irinaandonova <irinaandonova97@gmail.com>
2022-02-23 11:55:26 +0200

    Add add to cart functionality and add comments to product

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,15 +1,24 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
 import * as productService from "../../services/productsService.js";
+import Comment from '../Comment/Comment.js';
+import { addToCart } from '../../features/cartSlice.js';
+
 const Details = () => {
     const [product, setProduct] = useState({})
     const [quantity, setQuantity] = useState(1);
+    const [comments, setComments] = useState([]);
+
     const { productId } = useParams();
-    const plus = <FontAwesomeIcon icon={faPlus}/>;
-    const minus = <FontAwesomeIcon icon={faMinus}/>;
-    console.log(productId);
+    const dispatch = useDispatch();
+    const plus = <FontAwesomeIcon icon={faPlus} />;
+    const minus = <FontAwesomeIcon icon={faMinus} />;
+
     const quantityHander = (type) => {
         if (type === "decrease") {
             quantity > 1 && setQuantity(quantity - 1);
@@ -17,13 +26,17 @@ const Details = () => {
             setQuantity(quantity + 1);
         }
     }
-
     useEffect(() => {
         productService.getProduct(productId)
-                      .then(response => setProduct(response))
-                      .catch(err => console.log(err))
-    },[productId])
-    
+            .then(response => setProduct(response))
+            .then(response => setComments(response.comments))
+            .catch(err => console.log(err))
+    }, [productId])
+
+    const addToCartHandler = () => {
+        dispatch(addToCart({...product, quantity}));
+        alert('Успешно закупуване!');
+    }
 
 
     return (
@@ -40,9 +53,18 @@ const Details = () => {
                             <span className="quantity">{quantity}</span>
                             <button onClick={() => quantityHander('increase')}>{plus}</button>
                         </article>
-                        <button className="button" >Купи</button>
+                        <button className="button" onClick={addToCartHandler}>Купи</button>
+                        <button className="button">Добави коментар</button>
                     </article>
                 </div>
+            </article>
+            <article className="comments-wrapper">
+                {comments.length > 0
+                    ?
+                    comments.map(x => <Comment comment={x} key={x._id} />)
+                    :
+                    <p className='no-comments'>No comments for this product yet</p>
+                }
             </article>
         </section>
     )
