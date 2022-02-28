5884a6323808ef9e30e9807a4679309823db67c8
irinaandonova <irinaandonova97@gmail.com>
2022-02-28 19:25:20 +0200

    Add cart controller to routes

--- server/router.js
+++ server/router.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const authController = require('./controllers/authController.js');
 const productController = require('./controllers/productController.js');
 const profileController = require('./controllers/profileController.js');
+const cartController = require('./controllers/cartController.js');
 
 router.use('/auth', authController);
 router.use('/product', productController);
 router.use('/profile', profileController);
-
+router.use('/cart', cartController)
 module.exports = router;
