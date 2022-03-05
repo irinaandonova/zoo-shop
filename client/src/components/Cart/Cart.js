224bba55470e37ce51e4389109d9ec68a3406d54
irinaandonova <irinaandonova97@gmail.com>
2022-03-05 18:29:41 +0200

    Ad create order functionality

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,14 +1,32 @@
-import { useSelector } from "react-redux";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import AuthContext from "../../context/AuthContext.js";
+import cartService from "../../services/cartService.js";
 import Item from "./Item/Item.js";
 const Cart = () => {
-   const cart = useSelector((state) => state.cart);
-      return (
+    const cart = useSelector((state) => state.cart);
+    const navigate = useNavigate()
+    const { userInfo } = useContext(AuthContext);
+    console.log(`cart ${cart}`)
+    const createOrderHandler = () => {
+        let response = cartService.createOrder({ order: cart, user: userInfo._id });
+        if(response === 'ok') {
+        navigate('/');
+        }
+        else {
+            console.log(response);
+            console.log('error');
+        }
+    }
+
+    return (
         <section className="cart-wrapper">
             <ul className="cart-items-list">
                 {cart.cartItems.map(x => <Item item={x} key={x._id} />)}
             </ul>
             <p className="item">Обща сума: {(cart.totalPrice).toFixed(2)}лв</p>
-            <button >Завърши поръчката</button>
+            <button onClick={createOrderHandler}>Завърши поръчката</button>
         </section>
     )
 }
