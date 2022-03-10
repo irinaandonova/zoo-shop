b8e4720cb13c2589024900c28facc3c5a270c8bd
irinaandonova <irinaandonova97@gmail.com>
2022-03-10 19:33:10 +0200

    Fix finish order logic

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import AuthContext from "../../context/AuthContext.js";
 import cartService from "../../services/cartService.js";
 import Item from "./Item/Item.js";
@@ -8,11 +8,11 @@ const Cart = () => {
     const cart = useSelector((state) => state.cart);
     const navigate = useNavigate()
     const { userInfo } = useContext(AuthContext);
-    console.log(`cart ${cart}`)
-    const createOrderHandler = () => {
-        let response = cartService.createOrder({ order: cart, user: userInfo._id });
-        if(response === 'ok') {
-        navigate('/');
+    const createOrderHandler = async () => {
+        let response = await cartService.createOrder({ order: cart, user: userInfo._id });
+        console.log(response)
+        if (response.status === 'ok') {
+            navigate('/');
         }
         else {
             console.log(response);
@@ -22,9 +22,18 @@ const Cart = () => {
 
     return (
         <section className="cart-wrapper">
-            <ul className="cart-items-list">
+            <table className="cart-items-table">
+                <thead className="items-list">
+                    <tr>
+                    <td>Снимка</td>
+                    <td>Име на продукта</td>
+                    <td>Kоличество</td>
+                    <td>Единична цена</td>
+                    <td>Общо</td>
+                    </tr>
+                </thead>
                 {cart.cartItems.map(x => <Item item={x} key={x._id} />)}
-            </ul>
+            </table>
             <p className="item">Обща сума: {(cart.totalPrice).toFixed(2)}лв</p>
             <button onClick={createOrderHandler}>Завърши поръчката</button>
         </section>
