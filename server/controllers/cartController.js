4bb6948ff7b8fe19a1b40a85ededc37f5dee052d
irinaandonova <irinaandonova97@gmail.com>
2022-03-10 19:35:04 +0200

    Change response

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const cartService  = require('../services/cartService.js');
 const Cart = require('../models/Cart.js');
 
-router.post('', (req, res) => {
+router.post('', async(req, res) => {
     const { order, user } = req.body;
     console.log(user);
     console.log(`order ${order}`)
-    let result = cartService.createOrder({order, user});
+    let result = await cartService.createOrder({order, user});
     console.log(result)
-    res.json(result)
+    res.json({status: result});
 })
 
 module.exports = router;
