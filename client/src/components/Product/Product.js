3ac0c9af5a8afe621d31f1385754c0886cc4a839
irinaandonova <irinaandonova97@gmail.com>
2022-04-01 20:38:46 +0300

    Add class names for paragraphs

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 const Product = ( {product} ) => {
     return (
         <li className="product">
-            <p>Продукт: {product.productName}</p>
-            <p>Цена: {product.price}</p>
+            <p className="product-name">Продукт: {product.productName}</p>
+            <p className="product-price">Цена: {product.price}</p>
             <p className="img"><img src={product.imageUrl} alt="product" /></p>
-            <Link className="button" to={`/details/${product._id}`}>Детайли</Link>
+            <Link className="button details" to={`/details/${product._id}`}>Детайли</Link>
         </li>
     );
 }
