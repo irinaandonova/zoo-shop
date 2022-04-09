2e7f384e2d56d1884361c766a53262906e8d7167
irinaandonova <irinaandonova97@gmail.com>
2022-04-09 21:59:29 +0300

    Finish add comment functionality in addComment component

--- client/src/components/Comment/AddComment.js
+++ client/src/components/Comment/AddComment.js
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../context/AuthContext.js";
+import * as commentService from "../../services/commentService.js";
 const AddComment = () => {
-    const addCommentHandler = (e) => {
+    const { userInfo } = useContext(AuthContext);
+
+    const addCommentHandler = async(e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const comment = formData.get('comment');
-        console.log(comment);
+
+        try {
+            await commentService.addComment({ user: userInfo._id,comment });
+        }
+        catch(err) {
+            console.log(err);
+        }
     }
     return(
         <section className="add-comment-article">
