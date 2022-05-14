af0a3106af802e85730d1f5e5474c05e85a29dab
irinaandonova <irinaandonova97@gmail.com>
2022-05-14 11:41:50 +0300

    Parse response from server in authService

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -29,15 +29,20 @@ const login = async ({ email, password }) => {
     return result;
 }
 const editProfile = async({_id, user}) => {
-    let response = fetch(`${baseUrl}/${_id}`, {
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        method: 'POST', 
-        body: JSON.stringify({user})
-    })
-
-    return response;
+    try {
+        let response = await fetch(`${baseUrl}/${_id}`, {
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            method: 'POST', 
+            body: JSON.stringify(user)
+        })
+        let result = await response.json();
+        return result;
+    }
+    catch(err) {
+        console.log('Request error', err);
+    }
 }
 
 const authService = {
