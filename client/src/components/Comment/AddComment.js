9e15174f22cdf50e42e785fbbda55801f4b371ec
irinaandonova <irinaandonova97@gmail.com>
2022-05-22 16:27:28 +0300

    Create AddComments component

--- client/src/components/Comment/AddComment.js
+++ client/src/components/Comment/AddComment.js
@@ -1,31 +1,37 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import AuthContext from "../../context/AuthContext.js";
-import * as commentService from "../../services/commentService.js";
-const AddComment = () => {
-    const { userInfo } = useContext(AuthContext);
+import { addComment } from "../../features/commentsSlice.js";
+import * as productService from "../../services/productsService.js";
 
-    const addCommentHandler = async(e) => {
+const AddComment = (productId, comments) => {
+    const { userInfo } = useContext(AuthContext);
+    const dispatch = useDispatch();
+    
+    const addCommentHandler = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
-        const comment = formData.get('comment');
-
+        const text = formData.get('text');
+            
         try {
-            await commentService.addComment({ user: userInfo._id,comment });
+            await productService.addComment({ text, userId: userInfo._id, username: userInfo.username, productId });
+            let comment = { text, userId: userInfo._id, username: userInfo.username, productId };
+            dispatch(addComment({productId, comment}))
+            return;
         }
-        catch(err) {
+        catch (err) {
             console.log(err);
         }
-    }
-    return(
+    }  
+    return (
         <section className="add-comment-article">
             <form onSubmit={addCommentHandler} className="add-comment-form">
-            <textarea className="add-comment" name="comment">
-            </textarea>
-            <button className="button comment-btn">Добавяне</button>
+                <textarea className="add-comment" name="text">
+                </textarea>
+                <button className="button comment-btn">Добавяне</button>
             </form>
         </section>
     )
 }
 
-export default  AddComment;
+export default AddComment;
