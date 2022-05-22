0119384e1e44e28912804f4417c51c3b77539643
irinaandonova <irinaandonova97@gmail.com>
2022-05-22 15:27:18 +0300

    Add 'add comment' service in productService..js (server side)

--- server/services/productService.js
+++ server/services/productService.js
@@ -1,28 +1,29 @@
 const Product = require("../models/Product.js")
+const Comment = require("../models/Comment.js");
 
 const getAll = async () => {
     try {
         let result = await Product.find({}).lean();
-        return  result;
+        return result;
     }
     catch (err) {
         console.log(err);
-        return {status: 'err'};
+        return { status: 'err' };
     }
 }
 
 const getOne = async (id) => {
     try {
         let product = await Product.findById(id).lean();
-        return  product;
+        return product;
     }
     catch (err) {
         console.log(err);
-        return {status: 'err'};
+        return { status: 'err' };
     }
 }
 
-const getByType = async(animal) => {
+const getByType = async (animal) => {
     try {
         const allProducts = await getAll();
         const products = [];
@@ -35,14 +36,29 @@ const getByType = async(animal) => {
     }
     catch (err) {
         console.log(err);
-        return {status: 'err'};
+        return { status: 'err' };
+    }
+}
+
+const addComment = async ( userId, username, text, productId ) => {
+    
+    try {
+        let product = await Product.findById(  productId );
+        product.comments.push({userId, username, text, productId});
+        await product.save();
+        return { status: 'ok' };
+    }
+    catch (err) {
+        console.log(err);
+        return { status: 'err' };
     }
 }
 
 const productService = {
     getAll,
     getByType,
-    getOne
+    getOne,
+    addComment
 }
 
 module.exports = productService;
