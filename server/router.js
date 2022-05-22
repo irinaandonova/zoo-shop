8256e234bf893026550f5c2c90cf605b13435087
irinaandonova <irinaandonova97@gmail.com>
2022-05-22 15:25:12 +0300

    Delete comments controller and service and remove comments path in router

--- server/router.js
+++ server/router.js
@@ -3,11 +3,9 @@ const router = require('express').Router();
 const authController = require('./controllers/authController.js');
 const productController = require('./controllers/productController.js');
 const cartController = require('./controllers/cartController.js');
-const commentController = require('./controllers/commentController.js');
 
 router.use('/auth', authController);
 router.use('/product', productController);
 router.use('/cart', cartController);
-router.use('/comment', commentController);
 
 module.exports = router;
