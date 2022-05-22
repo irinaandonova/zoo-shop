c18625fcd05acf6a707a44a2a35371393720a75f
irinaandonova <irinaandonova97@gmail.com>
2022-05-22 16:10:30 +0300

    Add 'get comments' and 'add comments' reducers

--- client/src/features/commentsSlice.js
+++ client/src/features/commentsSlice.js
@@ -4,8 +4,22 @@ const commentsSlice = createSlice({
     name: "comments",
     initialState,
     reducers: {
-        
+        getComments: (state, action) => {
+            if(state.productId !== action.payload.productId) {
+                state.productId = action.payload.productId;                
+            }
+            state.comments = action.payload.comments;
+            console.log(state.comments);
+        },
+        addComment: (state, action) => {
+            if(state.productId !== action.payload.productId) {
+                getComments({productId: action.payload.productId, comments:action.payload.comments})
+            }
+            console.log(state.comments);
+            state.comments.push(action.payload.comment);
+        },
     }  
 })
 
+export const { getComments, addComment } = commentsSlice.actions;
 export default commentsSlice.reducer;
