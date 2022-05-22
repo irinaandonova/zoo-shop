c023879d23c8633bf76bbfae9f511e0c599390ed
irinaandonova <irinaandonova97@gmail.com>
2022-05-22 16:29:21 +0300

    Use redux in Details component

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,33 +1,30 @@
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 import * as productService from "../../services/productsService.js";
-import * as commentService from "../../services/commentService";
 
 import { addToCart } from '../../features/cartSlice.js';
 import AddComment from '../Comment/AddComment.js';
 import Comment from '../Comment/Comment.js';
 import AuthContext from '../../context/AuthContext.js';
+import { getComments } from '../../features/commentsSlice.js';
 
 const Details = () => {
     const [product, setProduct] = useState({})
     const [quantity, setQuantity] = useState(1);
-    const [comment, setComment] = useState(false);
-    const [comments, setComments] = useState({});
-
     const navigate = useNavigate();
-
     const { productId } = useParams();
     const { userInfo } = useContext(AuthContext);
 
     const dispatch = useDispatch();
-
+    const comments = useSelector((state) => state.comments.comments);
     const plus = <FontAwesomeIcon icon={faPlus} />;
     const minus = <FontAwesomeIcon icon={faMinus} />;
+
 
     const quantityHander = (type) => {
         if (type === "decrease") {
@@ -39,29 +36,23 @@ const Details = () => {
 
     useEffect(() => {
         productService.getProduct(productId)
-            .then(response => setProduct(response))
+            .then(response => {
+                setProduct(response)
+                dispatch(getComments({productId, comments: response.comments}))
+            })
             .catch(err => console.log(err))
-    }, [productId]);
+    }, [productId,dispatch]);
 
-    useEffect(() => {
-        commentService.getAll({productId})
-            .then(response => setComments(response))
-            .catch(err => console.log(err))
-    }, [productId])
 
     const addToCartHandler = () => {
-        if(userInfo._id) {
-        dispatch(addToCart({ ...product, quantity }));
-        alert('Успешно закупуване!');
+        if (userInfo._id) {
+            dispatch(addToCart({ ...product, quantity }));
+            alert('Успешно закупуване!');
         }
         else {
             navigate('/auth/login')
         }
     }
-    const addComment = () => {
-        setComment(true); 
-    }
-    console.log(product.comments)
 
     return (
         <section className="product-details">
@@ -78,16 +69,17 @@ const Details = () => {
                             <button onClick={() => quantityHander('increase')}>{plus}</button>
                         </article>
                         <button className="button" id='buy-btn' onClick={addToCartHandler}>Купи</button>
-                        <button className="button" onClick={addComment}>Добави коментар</button>
                     </article>
                 </div>
             </article>
             <article className="comments-wrapper">
-            {comment ? <AddComment productId={product._id}/> : null}
-            {comments.length > 0 ? 
-            comments.map(x => <Comment key={x._id} comment={x}/>)
-            :
-            <p>Няма коментари</p>}
+                {userInfo.email ? <AddComment  productId={productId} comments={comments}/> : null}
+                
+                {console.log(`component ${comments}`)}
+                {comments.length > 0 ?
+                comments.map((x, index) => <Comment key={productId + index} comment={x}/>)
+                :
+                <p>Няма коментари</p>}
             </article>
         </section>
     )
