4bacd946b97fab6730856d8dddf9903c6d7a4e6d
irinaandonova <irinaandonova97@gmail.com>
2022-05-22 16:34:49 +0300

    Move 'add comment' service in productService.js and delete commentsService.js (client side)

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,20 +1,15 @@
 const baseUrl = 'http://localhost:4000/comment';
 
-
-
-exports.addComment = async({user, comment }) => {
+exports.getAll = async() => {
     try {
-        let response = await fetch(`${baseUrl}/add`, {
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify({user, comment })
-        })
-        let result = await response.json();
-        return result;
+        let response = await fetch(`${baseUrl}/all`);
+        let all = await response.json();
+        return all;
     }
     catch(err) {
-        throw new Error(err);
+        console.log(err);
+        return 'Couldnot get comments'
     }
 }
+
+
