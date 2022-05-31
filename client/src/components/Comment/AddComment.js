ed39154dbd2b2711e640b17f945be403336e0983
irinaandonova <irinaandonova97@gmail.com>
2022-05-31 11:29:43 +0300

    Create add comment component

--- client/src/components/Comment/AddComment.js
+++ client/src/components/Comment/AddComment.js
@@ -2,27 +2,32 @@ import { useContext } from "react";
 import { useDispatch } from "react-redux";
 import AuthContext from "../../context/AuthContext.js";
 import { addComment } from "../../features/commentsSlice.js";
-import * as productService from "../../services/productsService.js";
+import * as commentService from "../../services/commentService.js";
 
-const AddComment = (productId, comments) => {
+const AddComment = (productId) => {
     const { userInfo } = useContext(AuthContext);
     const dispatch = useDispatch();
-    
+
     const addCommentHandler = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const text = formData.get('text');
-            
+
         try {
-            await productService.addComment({ text, userId: userInfo._id, username: userInfo.username, productId });
-            let comment = { text, userId: userInfo._id, username: userInfo.username, productId };
-            dispatch(addComment({productId, comment}))
+            let response = await commentService.addComment({ productId: Object.values(productId)[0], userId: userInfo._id, username: userInfo.username, text });
+            if (response.status === 'ok') {
+                let comment = response.comment;
+                dispatch(addComment({ productId, comment }))
+            }
+            else {
+                throw new Error('Couldnot add comment!')
+            }
             return;
         }
         catch (err) {
             console.log(err);
         }
-    }  
+    }
     return (
         <section className="add-comment-article">
             <form onSubmit={addCommentHandler} className="add-comment-form">
