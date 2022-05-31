d1e74c17dc2d20d5714270645fc8aba47247121d
irinaandonova <irinaandonova97@gmail.com>
2022-05-31 11:18:30 +0300

    Create comment controller in server side

---
+++ server/controllers/commentController.js
@@ -0,0 +1,31 @@
+const commentService = require('../services/commentService.js');
+const router = require('express').Router();
+
+router.post('/:productId/add', async (req, res) => {
+    const { userId, productId, text, username } = req.body;
+    try {
+        let response = await commentService.addComment({productId, userId, username, text});
+        res.json({ status: response.status, comment: response.newComment });
+    }
+    catch (err) {
+        console.log(err);
+        return { status: 'error' }
+    }
+
+});
+router.post('/:commentId/delete', async(req, res) => {
+    const { commentId } = req.params;
+    const { productId } = req.body;
+    console.log(productId);
+
+    try {
+        let response = await commentService.deleteComment(commentId, productId);
+        res.status(201).json(response);
+    }
+    catch (err) {
+        console.log(err);
+        return { status: 'error' }
+    }  
+})
+
+module.exports = router;
