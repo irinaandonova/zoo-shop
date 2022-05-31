b49e4952c42624e1547816eff89aa64a02ea0f16
irinaandonova <irinaandonova97@gmail.com>
2022-05-31 11:08:20 +0300

    Add comments route in server

--- server/router.js
+++ server/router.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const authController = require('./controllers/authController.js');
 const productController = require('./controllers/productController.js');
+const commentController = require('./controllers/commentController.js');
 const cartController = require('./controllers/cartController.js');
 
 router.use('/auth', authController);
 router.use('/product', productController);
+router.use('/comments', commentController)
 router.use('/cart', cartController);
 
 module.exports = router;
