2083c036b5a0d240f79d55a8e1e6b1626b172cd4
irinaandonova <irinaandonova97@gmail.com>
2022-06-15 11:59:35 +0300

    Change error responses

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,33 +4,34 @@ const productService = require('../services/productService.js');
 router.get('', async (req, res) => {
     try {
         let products = await productService.getAll();
-        res.status(201).json(products)
+
+        res.json({ status: 'ok', products });
     }
     catch (err) {
         console.log(err);
-        return res.status(501);
+        return res.json( { status: 'err' });
     }
 })
 router.get('/details/:_id', async (req, res) => {
     const id = req.params._id;
-
     let product = await productService.getOne(id);
-    let result = JSON.stringify(product);
 
+    let result = JSON.stringify(product);
     res.json(result)
 });
 router.get('/:animal', async (req, res) => {
     const animal = req.params.animal;
     const animals = ['dog', 'cat', 'roden', 'other'];
-    console.log('here')
+
     if (!animals.includes(animal)) {
-        res.status(404);
+        res.json({ status: 'error' });
+
         return;
     }
     try {
         let products = await productService.getByType(animal);
-        console.log(products)
-        res.status(201).json(products);
+        
+        res.json({status: 'ok', products });
     }
     catch (err) {
         console.log('g');
