ff9027f12fc39fe8947d4b82fe7cfdf40d6abf24
irinaandonova <irinaandonova97@gmail.com>
2022-06-15 17:18:38 +0300

    Change array name

--- client/src/features/commentsSlice.js
+++ client/src/features/commentsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialState =  {productId: '', commentsArr: []};
+const initialState =  {productId: '', comments: []};
 const commentsSlice = createSlice({
     name: "comments",
     initialState,
@@ -8,16 +8,14 @@ const commentsSlice = createSlice({
             if(state.productId !== action.payload.productId) {
                 state.productId = action.payload.productId;                
             }
-            state.commentsArr = action.payload.commentsArr;
-            console.log(state.commentsArr[0]);
+            state.comments = action.payload.comments;
         },
         addComment: (state, action) => {
-            state.commentsArr.push(action.payload.comment);
+            state.comments.push(action.payload.comment);
         },
         deleteComment: (state, action) => {
-            let index = state.commentsArr.indexOf(action.payload._id);
-
-            state.commentsArr.splice(index, 1);
+            let index = state.comments.indexOf(action.payload._id);
+            state.comments.splice(index, 1);
         }
     }  
 })
