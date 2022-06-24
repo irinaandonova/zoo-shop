7d56bf87be47a789dc845a039521f53bdf63359f
irinaandonova <irinaandonova97@gmail.com>
2022-06-24 19:27:46 +0300

    Add ratingSlice in redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from '../features/cartSlice.js';
 import commentsSlice from '../features/commentsSlice.js';
+import ratingSlice from '../features/ratingSlice.js';
 
 export const store = configureStore({
     reducer: {
         cart: cartSlice,
-        comments: commentsSlice
+        comments: commentsSlice,
+        rating: ratingSlice
     }
 })
