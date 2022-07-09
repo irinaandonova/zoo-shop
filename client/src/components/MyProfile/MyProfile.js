76976d5024e58df15f498f321ee7cfc3395df24d
irinaandonova <irinaandonova97@gmail.com>
2022-07-09 16:31:08 +0300

    Add username to component

--- client/src/components/MyProfile/MyProfile.js
+++ client/src/components/MyProfile/MyProfile.js
@@ -4,19 +4,19 @@ import AuthContext from "../../context/AuthContext.js";
 
 const MyProfile = () => {
    const { userInfo } = useContext(AuthContext);
-    const _id = userInfo._id;
+   const _id = userInfo._id;
     return(
         <section className="info-wrapper">
-                        <form className="profile-form">
+            <form className="profile-form">
             <label htmlFor="name">Име: {userInfo.firstName} {userInfo.lastName}</label>
             <label htmlFor="email">Имейл: {userInfo.email}</label>
-            <label htmlFor="name">Град: {userInfo.town}</label>
-            <label htmlFor="name">Адрес: {userInfo.address}</label>
-            <label htmlFor="name">Тел. номер: {userInfo.phoneNumber}</label>
+            <label htmlFor="username">Потребителско име: {userInfo.username}</label>
+            <label htmlFor="town">Град: {userInfo.town}</label>
+            <label htmlFor="address">Адрес: {userInfo.address}</label>
+            <label htmlFor="phoneNumber">Тел. номер: {userInfo.phoneNumber}</label>
             </form>
             <Link to={`/profile/${_id}/edit`} className="profile edit">Редакция на профил</Link>
         </section>
     )
 }
-
 export default MyProfile;
