d813a691f121ce36a19752ead527adcdc699e1d8
irinaandonova <irinaandonova97@gmail.com>
2022-07-19 18:17:03 +0300

    Create Card Payment coponent

---
+++ client/src/components/CardPayment/CartPayment.js
@@ -0,0 +1,22 @@
+const CardPayment = () => {
+    return(
+        <article className="card-payment">
+            <form className="card-payment-form">
+            <label htmlFor="card-number">Номер на картата:</label>
+            <input name="card-number" className="payment-fiels"/>
+            <label htmlFor="name">Имена:</label>
+            <input name="name" className="payment-fiels"/>
+            <label htmlFor="valid-thru">Валидна до:</label>
+            <input name="valid-thru" className="payment-fiels"/>
+            <label htmlFor="CVC">CVC</label>
+            <input name="CVC" className="payment-fiels"/>
+            <article className="payment-form-btn">
+            <button type="submit" className="submit-btn">Предаване</button>
+            <button className="reset-btn">Изчистване на полетата</button>
+            </article>
+            </form>
+        </article>
+    )
+}
+
+export default CardPayment;
