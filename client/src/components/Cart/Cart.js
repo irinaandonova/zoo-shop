bdbd93fae9aa41975a963406e9e22f38585fa4b2
irinaandonova <irinaandonova97@gmail.com>
2022-07-29 12:37:22 +0300

    Delete duplicate code

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,10 +1,9 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import emailjs from "@emailjs/browser";
 import AuthContext from "../../context/AuthContext.js";
-import cartService from "../../services/cartService.js";
 import Item from "./Item/Item.js";
+import { useSelector } from "react-redux";
+
 const Cart = () => {
     const cart = useSelector((state) => state.cart);
     const navigate = useNavigate();
@@ -13,7 +12,6 @@ const Cart = () => {
 
     const createOrderHandler = async (e) => {
         e.preventDefault();
-
         if (!isAuthenticated) {
             navigate('/auth/login');
         }
@@ -23,32 +21,7 @@ const Cart = () => {
                 alert('Нямате продукти в количката!');
                 return;
             }
-            let response = await cartService.createOrder({ order: cart, userId: userInfo._id });
-
-            let timestamp = new Date(response.cart.createdAt);
-            timestamp.setDate(timestamp.getDate() + 2);
-
-            let deliveryDate = timestamp.toLocaleDateString('bg-BG', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-            });
-
-            let template_params = { to_name: userInfo.firstName, orderId: response.cart._id, deliveryDate, to_email: userInfo.email }
-            if (response.status === 'ok') {
-                emailjs.send('service_2v0jqnu', 'template_uhrspdh', template_params, 'KoIx4k6LBbfML5ZXa')
-                    .then((result) => {
-                        userInfo.hasOrder = true;
-                        navigate('/cart')
-                    }, (error) => {
-                        console.log(error.text);
-                    });
-            }
-            else {
-                if (!userInfo._id) {
-                    navigate('/auth/login')
-                }
-            }
+            navigate('/cart/order-info');           
         }
         catch (err) {
             console.log(err);
