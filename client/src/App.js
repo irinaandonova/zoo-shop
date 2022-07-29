66c070691f83fa7b253cef830c3c3d429322633c
irinaandonova <irinaandonova97@gmail.com>
2022-07-29 12:30:21 +0300

    Add new route

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import MyProfile from './components/MyProfile/MyProfile.js';
 import EditProfile from './components/Edit Profile/EditProfile.js';
 import Details from './components/Details/Details.js';
 import Cart from './components/Cart/Cart.js';
+import OrderInfo from './components/OrderInfo/OrderInfo.js';
 function App() {
   return (
     <div className="App">
@@ -24,8 +25,9 @@ function App() {
           <Route path='/other' element={<Dashboard />} />
           <Route path='/profile' element={<MyProfile />} />
           <Route path="/profile/:_id/edit" element={<EditProfile />} />
-            <Route path='/details/:productId' element={<Details />} />
+          <Route path='/details/:productId' element={<Details />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/cart/order-info' element={OrderInfo}/>
         </Routes>
       </AuthContextProvider>
     </div>
