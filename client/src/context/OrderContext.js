e4c669f18d0aa86942b4b0603a1f16d823a13337
irinaandonova <irinaandonova97@gmail.com>
2022-07-30 15:56:25 +0300

    Add cart to context

--- client/src/context/OrderContext.js
+++ client/src/context/OrderContext.js
@@ -3,11 +3,11 @@ import { createContext, useState } from "react";
 const OrderContext = createContext();
 
 export const OrderContextProvider = ({ children }) => {
-    const defaultValue = { userInfo: '', paymentMethod: '' };
+    const defaultValue = { userDetails: '', paymentMethod: '', cart: { cartItems: [], totalPrice: 0.00 } };
     const [orderInfo, setOrderInfo] = useState(defaultValue);
 
-    const getOrderInfo = (userInfo, paymentMethod) => {
-        setOrderInfo({ userInfo, paymentMethod });
+    const getOrderInfo = (userDetails, paymentMethod, cart) => {
+        setOrderInfo({ userDetails, paymentMethod, cart });
     }
     const clearOrderInfo = () => {
         setOrderInfo(defaultValue);
