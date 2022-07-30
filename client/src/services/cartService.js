0dbf9f585bf0120e5a3f84dc089ec6a55b8d8d69
irinaandonova <irinaandonova97@gmail.com>
2022-07-30 16:15:13 +0300

    Change paramethers

--- client/src/services/cartService.js
+++ client/src/services/cartService.js
@@ -1,21 +1,33 @@
+import emailjs from "@emailjs/browser";
+
 const baseUrl = 'http://localhost:4000/cart';
 
-const createOrder = async({ order, userId }) => {
+const createOrder = async ({ cart, userDetails, paymentMethod }) => {
     let response = await fetch(baseUrl, {
         headers: {
             'Content-Type': 'application/json'
         },
         method: 'POST',
-        body: JSON.stringify({order, userId})
+        body: JSON.stringify({ cart, userDetails, paymentMethod })
     });
 
     let result = await response.json();
 
     return result;
 }
+const sendEmail = (template_params) => {
+    emailjs.send('service_2v0jqnu', 'template_uhrspdh', template_params, 'KoIx4k6LBbfML5ZXa')
+        .then((result) => {
+            return true;
+        }, (error) => {
+            console.log(error.text);
+            return false;
+        });
 
+}
 const cartService = {
     createOrder,
+    sendEmail
 }
 
 export default cartService;
