40893b472ce02e0e1bf7370b5193ee07b288efa4
irinaandonova <irinaandonova97@gmail.com>
2022-09-01 19:29:22 +0300

    Create sumbit payment functionality in component

--- client/src/components/CardPayment/CartPayment.js
+++ client/src/components/CardPayment/CartPayment.js
@@ -3,16 +3,31 @@ const CardPayment = () => {
         e.preventDefault();
         e.target.reset();
     }
-    
+    const onSubmitCardPayment = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const cardNumber = formData.get('cardNumber');
+        const name = formData.get('name');
+        const validThru = formData.get('validThru');
+        const CVC = formData.get('CVC');
+
+        const cardInfo = {
+            cardNumber,
+            name,
+            validThru,
+            CVC
+        }
+        
+    }
     return (
         <article className="card-payment">
-            <form className="card-payment-form">
+            <form className="card-payment-form" onSubmit={onSubmitCardPayment}>
                 <label htmlFor="card-number">Номер на картата:</label>
-                <input name="card-number" className="payment-fiels" />
+                <input name="cardNumber" className="payment-fiels" />
                 <label htmlFor="name">Имена:</label>
                 <input name="name" className="payment-fiels" />
                 <label htmlFor="valid-thru">Валидна до:</label>
-                <input name="valid-thru" className="payment-fiels" />
+                <input name="validThru" className="payment-fiels" />
                 <label htmlFor="CVC">CVC</label>
                 <input name="CVC" className="payment-fiels" />
                 <article className="payment-form-btn">
