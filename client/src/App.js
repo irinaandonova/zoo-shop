adf3b9c485316601b60a58a782aa990a53e31143
irinaandonova <irinaandonova97@gmail.com>
2022-09-08 13:18:50 +0300

    Add route for stripe container

--- client/src/App.js
+++ client/src/App.js
@@ -10,30 +10,29 @@ import EditProfile from './components/Edit Profile/EditProfile.js';
 import Details from './components/Details/Details.js';
 import Cart from './components/Cart/Cart.js';
 import OrderInfo from './components/OrderInfo/OrderInfo.js';
-import { OrderContextProvider } from './context/OrderContext.js';
-import CardPayment from './components/CardPayment/CardPayment';
+import StripeContainer from './components/CardPayment/StripeContainer';
+
 function App() {
+
   return (
     <div className="App">
       <AuthContextProvider>
         <Header />
-        <OrderContextProvider>
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/auth/login' element={<Login />} />
-            <Route path='/auth/register' element={<Register />} />
-            <Route path='/cat' element={<Dashboard />} />
-            <Route path='/dog' element={<Dashboard />} />
-            <Route path='/roden' element={<Dashboard />} />
-            <Route path='/other' element={<Dashboard />} />
-            <Route path='/profile' element={<MyProfile />} />
-            <Route path="/profile/:_id/edit" element={<EditProfile />} />
-            <Route path='/details/:productId' element={<Details />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/cart/order-info' element={<OrderInfo />} />
-            <Route path='/cart/card-payment' element={<CardPayment />} />
-          </Routes>
-        </OrderContextProvider>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/auth/login' element={<Login />} />
+          <Route path='/auth/register' element={<Register />} />
+          <Route path='/cat' element={<Dashboard />} />
+          <Route path='/dog' element={<Dashboard />} />
+          <Route path='/roden' element={<Dashboard />} />
+          <Route path='/other' element={<Dashboard />} />
+          <Route path='/profile' element={<MyProfile />} />
+          <Route path="/profile/:_id/edit" element={<EditProfile />} />
+          <Route path='/details/:productId' element={<Details />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/cart/order-info' element={<OrderInfo />} />
+          <Route path='/cart/card-payment' element={<StripeContainer />} />
+        </Routes>
       </AuthContextProvider>
     </div>
   );
