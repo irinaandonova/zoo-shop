b7d09f88e6353ce86ec8ae44b38a8385d7b9aad6
irinaandonova <irinaandonova97@gmail.com>
2022-09-08 13:30:27 +0300

    Add try/catch block to request and create checkoutPayment request

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -1,21 +1,26 @@
 const baseUrl = 'http://localhost:4000/order';
 
-const createOrder = async ({ cart, userDetails, paymentMethod }) => {
-    let response = await fetch(baseUrl, {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({ cart, userDetails, paymentMethod })
-    });
+const createOrder = async ({ cart, user, paymentMethod, cardInfo, line_items }) => {
+    try {
+        let response = await fetch(`${baseUrl}/create-checkout-session`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ cart, user, paymentMethod, cardInfo, line_items })
+        });
 
-    let result = await response.json();
-
-    return result;
+        let result = await response.json();
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+        return 'err';
+    }
 }
 const checkoutPayment = async ({ cardInfo }) => {
     try {
-        let response = await fetch(`${baseUrl}/checkout`, {
+        let response = await fetch(`${baseUrl}/create-checkout-session`, {
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -28,6 +33,7 @@ const checkoutPayment = async ({ cardInfo }) => {
     }
     catch (err) {
         console.log(err);
+
         return { status: 'err' };
     }
 }
