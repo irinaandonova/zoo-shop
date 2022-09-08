a3da2f0ad65679a1c8f88ec29676240ad9afa2d6
irinaandonova <irinaandonova97@gmail.com>
2022-09-08 18:53:44 +0300

    Import bootstrap style in index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
