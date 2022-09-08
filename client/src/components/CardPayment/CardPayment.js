c1539387dda3fde0d885a6568b63584f6f88ad2f
irinaandonova <irinaandonova97@gmail.com>
2022-09-08 13:21:55 +0300

    Map cartItems to stripe format and send email if server respond with ok

--- client/src/components/CardPayment/CardPayment.js
+++ client/src/components/CardPayment/CardPayment.js
@@ -1,14 +1,17 @@
+import { useStripe } from "@stripe/react-stripe-js";
 import { useContext } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import orderService from "../../services/orderService";
+import  convertTime  from "../../helpers/timeHelper";
+import sendEmail from "../../helpers/emailHelper";
 
 const CardPayment = () => {
     const navigate = useNavigate();
     const cart = useSelector((state) => state.cart);
     const { userInfo } = useContext(AuthContext);
-
+    const stripe = useStripe();
     const onResetHandler = (e) => {
         e.preventDefault();
         e.target.reset();
@@ -27,10 +30,32 @@ const CardPayment = () => {
             validThru,
             CVC
         }
+        const line_items = cart.cartItems.map(item => {
+            return {
+                price_data: {
+                    currency: 'bgn',
+                    product_data: {
+                        name: item.productName,
+                    },
+                    unit_amount: item.price * 100                   
+                },
+                quantity: item.quantity
+            }
+        })
         try {
-            const response = await orderService.createOrder({ user: userInfo, cart, paymentMethod: 'card', cardInfo });
+            const response = await orderService.createOrder({ user: userInfo, cart, paymentMethod: 'card', cardInfo, line_items });
             if (response.status === 'ok') {
+                const deliveryDate = convertTime(response.createdAt, 2);
+                const template_params = { to_name: userInfo.firstName, orderId: response.cart._id, deliveryDate, to_email: userInfo.email }
+
+                await sendEmail(template_params);
+
+                userInfo.hasOrder = true;
                 navigate('/cart');
+            }
+            else {
+                console.log(response);
+                await stripe.redirectToCheckout({ sessionId: response.sessionId })
             }
         }
         catch (err) {
