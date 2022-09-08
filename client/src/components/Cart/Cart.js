6c56fb2863b45744e389b0f84f60a62a11b96e07
irinaandonova <irinaandonova97@gmail.com>
2022-09-08 18:51:11 +0300

    Add no items in cart alert

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,51 +1,64 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext.js";
 import Item from "./Item/Item.js";
 import { useSelector } from "react-redux";
+import Alerts from "../Common/Alerts/Alerts.js";
 
 const Cart = () => {
+    const [showAlert, setShowAlert] = useState(false);
     const cart = useSelector((state) => state.cart);
     const navigate = useNavigate();
 
     const { userInfo, isAuthenticated } = useContext(AuthContext);
-    
+
     const createOrderHandler = (e) => {
         e.preventDefault();
         if (!isAuthenticated) {
             navigate('/auth/login');
-        }        
-            if(cart.cartItems.length === 0) {
-                alert('No products in cart!');
-                return;
-            }
-
-            navigate('/cart/order-info');           
+        }
+        if (cart.cartItems.length === 0) {
+            setShowAlert(true);
+            return;
+        }
+        navigate('/cart/order-info');
     }
 
     return (
-        <section className="cart-wrapper">
+        <section>
             {userInfo.hasOrder ?
-                <p>Your order is being processed</p>
+                <section className="card-wrapper processing">
+                    <p>Your order is being processed</p>
+
+                </section>
                 :
-                <article>
-                    <table className="cart-items-table">
-                        <thead className="items-list">
-                            <tr>
-                                <td>Image</td>
-                                <td>Product Name</td>
-                                <td>Quantity</td>
-                                <td>Unit price</td>
-                                <td>Total</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {cart.cartItems.map(x => <Item item={x} key={x._id} />)}
-                        </tbody>
-                    </table>
-                    <p className="item">Total price: {(cart.totalPrice).toFixed(2)}lv</p>
+                <section className="cart-wrapper" >
+                    <article onClick={() => setShowAlert(false)}>
+                        <table className="cart-items-table">
+                            <thead className="items-list">
+                                <tr>
+                                    <td>Image</td>
+                                    <td>Product Name</td>
+                                    <td>Quantity</td>
+                                    <td>Unit price</td>
+                                    <td>Total</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cart.cartItems.map(x => <Item item={x} key={x._id} />)}
+                            </tbody>
+                        </table>
+                        <p className="item">Total price: {(cart.totalPrice).toFixed(2)}lv</p>
+
+                    </article>
                     <button className="button" onClick={createOrderHandler}>Finalize order</button>
-                </article>
+                    {
+                        showAlert ?
+                            <Alerts message={'No products in cart!'} />
+                            :
+                            null
+                    }
+                </section>
             }
 
         </section>
