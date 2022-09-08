4a89290b35f4ca9c24e67f6140e9b9c99c3c5e9b
irinaandonova <irinaandonova97@gmail.com>
2022-09-08 13:19:30 +0300

    Create stripe container component

--- client/src/components/CardPayment/StripeContainer.js
+++ client/src/components/CardPayment/StripeContainer.js
@@ -1,14 +1,14 @@
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import CardPayment from './CartPayment.js';
+import CardPayment from './CardPayment.js';
 
 const PUBLIC_KEY = 'pk_test_51LNLdyLJQrG77gZseHENc5Ci4dKstdnM0pBBJr8s1SyqHnyZMXZHIlsMMs0wgHhGPqpkNhLH29j65A4HsYfYYkEW00SLbhqVPw';
-const stripeTestPromise = loadStripe(PUBLIC_KEY);
+const stripePromise = loadStripe(PUBLIC_KEY);
 
 const StripeContainer = () => {
     return (
-        <Elements stripe={stripeTestPromise}>
-        <CardPayment/>
+        <Elements stripe={stripePromise}>
+            <CardPayment />
         </Elements>
     )
 }
