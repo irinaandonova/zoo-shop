abcc7f2497adde76f19d525316bf24b5066df495
irinaandonova <irinaandonova97@gmail.com>
2022-09-14 11:34:38 +0300

    Style some more

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -6,23 +6,30 @@ import Product from "../Product/Product.js";
 const Dashboard = () => {
     const [products, setProducts] = useState([]);
     const [filter, setFilter] = useState('alphabetical');
-    const [ subtype, setSubtype ] = useState('all');
+    const [subtype, setSubtype] = useState('all');
 
     const location = useLocation();
     const animal = location.pathname.split('/')[1];
-    
+
     useEffect(() => {
         productService.getProducts(animal, filter, subtype)
             .then(result => setProducts(result))
             .catch(err => console.log(err))
     }, [animal, filter, subtype]);
 
-    const subtypeHandler = async(e) => {
+    const subtypeHandler = async (e) => {
         await setSubtype(e.target.value);
-        
+
     }
     return (
         <section className="home-page" >
+            <article className="subtype-article">
+                <p className="subtype-p">Type product: </p>
+                <button className="subtype profile" onClick={subtypeHandler} value="all">All</button>
+                <button className="subtype profile" onClick={subtypeHandler} value="dry">Dry food</button>
+                <button className="subtype profile" onClick={subtypeHandler} value="canned">Canned food</button>
+                <button className="subtype profile" onClick={subtypeHandler} value="accessory">Accessory</button>
+            </article>
             <article className="filter-article">
                 <label htmlFor="filter" className="filter-label">Sort by:</label>
                 <select id="filter" onChange={(e) => setFilter(e.currentTarget.value)} defaultValue={filter}>
@@ -32,13 +39,6 @@ const Dashboard = () => {
                     <option value="price-lower">Price(acending)</option>
                 </select>
             </article>
-            <article className="subtype-article">
-                    <p className="subtype-p">Type product: </p>
-                    <button className="subtype profile" onClick={subtypeHandler} value="all">All</button>
-                    <button className="subtype profile" onClick={subtypeHandler} value="dry">Dry food</button>
-                    <button className="subtype profile" onClick={subtypeHandler} value="canned">Canned food</button>
-                    <button className="subtype profile" onClick={subtypeHandler} value="accessory">Accessory</button>
-                </article>
             <article className="products">
                 <ul className="all-products">
                     {products.length > 0 ?
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -23,23 +23,23 @@ const Header = () => {
                 <Link to="/" className="logo">ZOOLAND</Link>
                 {userInfo._id ?
                     <article className="user-header-article">
-                        <p className="greeting">Hello, {userInfo.firstName}</p>
+                        <p className="greeting left-link">Hello, {userInfo.firstName}</p>
                         <Link to="/profile" className="profile">My profile</Link>
-                        <input type="button" onClick={logoutHandler} value="Exit profile" className="profile" />
+                        <input type="button" onClick={logoutHandler} value="Exit profile" className="profile" id="exit"/>
                     </article>
                     :
                     <article className="guest-header-article">
-                        <Link to="/auth/login" className="profile">Login</Link>
+                        <Link to="/auth/login" className="profile left-link">Login</Link>
                         <Link to="/auth/register" className="profile">Register</Link>
                     </article>
                 }
             </article>
             <nav className="navbar">
                 <ul className="nav-list">
-                    <li><Link to="/dog" className="category-link">Dogs</Link></li>
+                    <li><Link to="/dog" className="category-link dog">Dogs</Link></li>
                     <li><Link to="/cat" className="category-link">Cats</Link></li>
                     <li><Link to="/roden" className="category-link">Rodens</Link></li>
-                    <li><Link to="/other" className="category-link">Other</Link></li>
+                    <li><Link to="/other" className="category-link other">Other</Link></li>
                     {
                         userInfo._id ?
                             <li><Link to="/cart" className="category-link"><FontAwesomeIcon icon={faShoppingCart} /></Link></li>
