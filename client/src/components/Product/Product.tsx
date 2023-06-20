9ff1aa1460766ea411c567f4747552851cb0590c
Irina Andonova <irinaandonova97@gmail.com>
2023-06-20 14:30:47 +0300

    MIgrate file to Typescript

--- client/src/components/Product/Product.tsx
+++ client/src/components/Product/Product.tsx
@@ -1,19 +1,18 @@
 import { useNavigate } from 'react-router-dom';
+import { IProduct } from '../../types';
 
-const Product = ({ product }) => {
-    const navigate = useNavigate();
-    
+const Product = (product: IProduct) => {
+    const navigate = useNavigate()
     const detailsHandler = () => {
         navigate(`/details/${product._id}`);
     }
-    
+
     return (
         <li className="product" onClick={detailsHandler}>
-            <p className="product-name">{product.productName}</p>            
+            <p className="product-name">{product.productName}</p>
             <p className="img"><img src={product.imageUrl} alt="product" /></p>
             <p className="product-price">{product.price}</p>
         </li>
     );
 }
-
 export default Product;
